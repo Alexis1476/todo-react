4daf3923b9a1a51e21d2375033b15395408016b4
Alexis1476 <halexisr20@hotmail.com>
2023-01-13 11:33:12 +0100

    Color white and black added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,9 @@ module.exports = {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        colors:{
+        colors: {
+            black: '#000',
+            white: '#FFF',
             'primary': '#4E69F4'
         },
         extend: {},
