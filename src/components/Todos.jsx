f9486ec0cb9bdfd77ed9db42f9bfef5da2f61ee6
Alexis1476 <halexisr20@hotmail.com>
2023-01-16 15:53:26 +0100

    Tasks unfinished counter

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,14 @@
 import Todo from "./Todo.jsx";
+import React from "react";
 
 function Todos({todos, handleToggle}) {
     return (
-        <ul className="grow h-2 overflow-auto">
-            {todos.map(todo => <Todo todo={todo} key={todo.id} name={todo.name} handleToggle={handleToggle}/>)}
-        </ul>
+        <>
+            <p className="mb-2">To do : {todos.filter(task => !task.completed).length}</p>
+            <ul className="grow h-2 overflow-auto">
+                {todos.map(todo => <Todo todo={todo} key={todo.id} name={todo.name} handleToggle={handleToggle}/>)}
+            </ul>
+        </>
     )
 }
 
