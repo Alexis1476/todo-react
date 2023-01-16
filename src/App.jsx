79ee9028fa157e8724844ed7b6dca2dd1a35fdf8
Alexis1476 <halexisr20@hotmail.com>
2023-01-16 15:34:41 +0100

    Style when task is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import React, {useState} from 'react'
-import './App.css'
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Todos from "./components/Todos.jsx";
 import TodoForm from "./components/TodoForm.jsx";
 
-const tasks = [
-    {id: 1, name: 'Task1', completed: true},
-    {id: 2, name: 'Task2', completed: false},
-    {id: 3, name: 'Task3', completed: false},
-]
-
 function App() {
-    const [todos, setTodos] = useState(tasks);
+    const [todos, setTodos] = useState([]);
     const addTask = (value) => {
         let copy = [...todos, {id: todos.length + 1, name: value}]
         setTodos(copy)
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,7 @@
 function Todo({todo, handleToggle}) {
     return (
         <li id={todo.id}
-            className={`rounded-md text-white px-4 py-2 mb-2 flex ${todo.completed ? 'bg-black' : 'bg-primary'}`}
+            className={`bg-primary rounded-md text-white px-4 py-2 mb-2 flex ${!todo.completed || 'line-through'}`}
             onClick={(e) => handleToggle(e.currentTarget.id)}>
             <strong>{todo.name}</strong>
         </li>
