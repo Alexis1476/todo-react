36074b2a449d782f0073cd39c88fedc6d8f4d6fc
Alexis1476 <halexisr20@hotmail.com>
2023-01-16 16:14:04 +0100

    Style updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button({href = '#', title, img, className}) {
     return (
-        <a href={href} title={title} className={`p-6 flex br-2 rounded-2xl ${className}`}>
+        <a href={href} title={title} className={`p-6 flex br-2 rounded-2xl hover:opacity-70 ${className}`}>
             <img src={img} alt=""/>
         </a>
     )
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,7 @@
 function Todo({todo, handleToggle}) {
     return (
         <li id={todo.id}
-            className={`bg-primary rounded-md text-white px-4 py-2 mb-2 flex ${!todo.completed || 'line-through'}`}
+            className={`bg-primary rounded-md text-white px-4 py-2 mb-2 flex cursor-pointer ${!todo.completed || 'line-through bg-gray-700'}`}
             onClick={(e) => handleToggle(e.currentTarget.id)}>
             <strong>{todo.name}</strong>
         </li>
