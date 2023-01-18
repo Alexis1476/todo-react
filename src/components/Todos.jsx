dd5b5b6d65dafffecedd4f4eff19023e568ef43a
Alexis1476 <halexisr20@hotmail.com>
2023-01-18 08:20:43 +0100

    Condition if there are 0 tasks

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Todos({todos, handleToggle}) {
     return (
         <>
-            <p className="mb-2">To do : {todos.filter(task => !task.completed).length}</p>
+            {todos.length <= 0 || <p className="mb-2">To do : {todos.filter(task => !task.completed).length}</p>}
             <ul className="grow h-2 overflow-auto">
                 {todos.map(todo => <Todo todo={todo} key={todo.id} name={todo.name} handleToggle={handleToggle}/>)}
             </ul>
